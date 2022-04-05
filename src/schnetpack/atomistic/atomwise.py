3f9c251c8aca7047ba10b5dffc25fe547361f556
Jonas Lederer <jonas.lederer@tu-berlin.de>
2022-04-05 11:49:14 +0200

    pdbbind pooling layer implemented (AtomwisePLC)

--- src/schnetpack/atomistic/atomwise.py
+++ src/schnetpack/atomistic/atomwise.py
@@ -8,7 +8,91 @@ import schnetpack as spk
 import schnetpack.nn as snn
 import schnetpack.properties as properties
 
-__all__ = ["Atomwise", "DipoleMoment", "Polarizability"]
+__all__ = ["AtomwisePLC", "Atomwise", "DipoleMoment", "Polarizability"]
+
+
+class AtomwisePLC(nn.Module):
+    """
+    Predicts atom-wise contributions and accumulates global prediction, e.g. for the energy.
+
+    If `aggregation_mode` is None, only the per-atom predictions will be returned.
+    """
+
+    def __init__(
+        self,
+        n_in: int,
+        n_out: int = 1,
+        n_hidden: Optional[Union[int, Sequence[int]]] = None,
+        n_layers: int = 2,
+        activation: Callable = F.silu,
+        aggregation_mode: str = "sum",
+        output_key: str = "y",
+        per_atom_output_key: Optional[str] = None,
+    ):
+        """
+        Args:
+            n_in: input dimension of representation
+            n_out: output dimension of target property (default: 1)
+            n_hidden: size of hidden layers.
+                If an integer, same number of node is used for all hidden layers resulting
+                in a rectangular network.
+                If None, the number of neurons is divided by two after each layer starting
+                n_in resulting in a pyramidal network.
+            n_layers: number of layers.
+            aggregation_mode: one of {sum, avg} (default: sum)
+            output_key: the key under which the result will be stored
+            per_atom_output_key: If not None, the key under which the per-atom result will be stored
+        """
+        super(AtomwisePLC, self).__init__()
+        self.output_key = output_key
+        self.model_outputs = [output_key]
+        self.per_atom_output_key = per_atom_output_key
+        self.n_out = n_out
+
+        if aggregation_mode is None and self.per_atom_output_key is None:
+            raise ValueError(
+                "If `aggregation_mode` is None, `per_atom_output_key` needs to be set,"
+                + " since no accumulated output will be returned!"
+            )
+
+        self.outnet = spk.nn.build_mlp(
+            n_in=n_in,
+            n_out=n_out,
+            n_hidden=n_hidden,
+            n_layers=n_layers,
+            activation=activation,
+        )
+        self.aggregation_mode = aggregation_mode
+
+    def forward(self, inputs: Dict[str, torch.Tensor]) -> Dict[str, torch.Tensor]:
+        # predict atomwise contributions
+        y = self.outnet(inputs["scalar_representation"])
+
+        # aggregate
+        if self.aggregation_mode is not None:
+            idx_m = inputs[properties.idx_m]
+
+            # adapt idx_m to distinguish between ligand, pocket and complex
+            idx_m_new = idx_m * 3
+            idx_m_new += inputs["pocket_separate_mask"].long()
+            idx_m_new += inputs["ligand_separate_mask"].long() * 2
+
+            # original aggregation procedure
+            maxm = int(idx_m_new[-1]) + 1
+            y = snn.scatter_add(y, idx_m_new, dim_size=maxm)
+            y = torch.squeeze(y, -1)
+
+            # model prediction is given by energy contribution difference between isolated pocket+ligand and complex
+            y_complex = y[::3]
+            y_pocket = y[1::3]
+            y_ligand = y[2::3]
+            y = y_pocket + y_ligand - y_complex
+
+            if self.aggregation_mode == "avg":
+                y = y / inputs[properties.n_atoms]
+
+        inputs[self.output_key] = y
+        return inputs
 
 
 class Atomwise(nn.Module):
