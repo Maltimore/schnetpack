1ad0766e9d175292a2036ce487aeadc4ca4fad70
Maltimore <git@maltimore.info>
2025-10-01 11:43:28 +0200

    bond angle terms. training now very slow though

--- src/schnetpack/representation/empiricalff.py
+++ src/schnetpack/representation/empiricalff.py
@@ -26,9 +26,16 @@ class EmpiricalFF(nn.Module):
         self.bonded_mask = loaded['bonded_mask']
         self.idx_i_bonded = loaded['idx_i_bonded']
         self.idx_j_bonded = loaded['idx_j_bonded']
+        self.idx_i_triples = loaded[properties.idx_i_triples]
+        self.idx_j_triples = loaded[properties.idx_j_triples]
+        self.idx_k_triples = loaded[properties.idx_k_triples]
 
-        self.pairwise_equilibrium = torch.nn.Parameter(torch.ones(self.idx_i_bonded.shape[0]) * 1.5)
-        self.pairwise_force_constant =  torch.nn.Parameter(torch.ones(self.idx_i_bonded.shape[0]))
+
+
+        self.bond_distance_equilibrium = torch.nn.Parameter(torch.ones(self.idx_i_bonded.shape[0]) * 1.5)
+        self.bond_distance_force_constant =  torch.nn.Parameter(torch.ones(self.idx_i_bonded.shape[0]))
+        self.bond_angle_equilibrium = torch.nn.Parameter(torch.ones(self.idx_j_triples.shape[0]) * 0.9)
+        self.bond_angle_force_constant =  torch.nn.Parameter(torch.ones(self.idx_j_triples.shape[0]))
 
 
 
@@ -39,11 +46,19 @@ class EmpiricalFF(nn.Module):
         D_ij_full = torch.norm(R_ij_full, dim=1)
         R_ij_bonded = R_ij_full[self.bonded_mask]
         D_ij_bonded = D_ij_full[self.bonded_mask]
+        energy_terms = []
 
         # bond lengths
-        E_bond = 0.5 * self.pairwise_force_constant * (D_ij_bonded - self.pairwise_equilibrium)**2
-        E_bond_atomwise = snn.scatter_add(E_bond, self.idx_i_bonded, dim_size=len(atomic_numbers), dim=0)
+        E_bond_distance = 0.5 * self.bond_distance_force_constant * (D_ij_bonded - self.bond_distance_equilibrium)**2
+        E_bond_distance_atomwise = snn.scatter_add(E_bond_distance, self.idx_i_bonded, dim_size=len(atomic_numbers), dim=0)
+        energy_terms.append(E_bond_distance_atomwise[:, None])
 
-        inputs["scalar_representation"] = E_bond_atomwise[:, None]
+        # bond angles
+        bond_angles = torch.einsum('bi,bi->b', R_ij_bonded[self.idx_j_triples], R_ij_bonded[self.idx_k_triples]) / (torch.linalg.norm(R_ij_bonded[self.idx_j_triples], dim=1) * torch.linalg.norm(R_ij_bonded[self.idx_k_triples], dim=1))
+        E_bond_angle = 0.33 * self.bond_angle_force_constant * (self.bond_angle_equilibrium - bond_angles)**2
+        E_bond_angle_atomwise = snn.scatter_add(E_bond_angle, self.idx_i_triples, dim_size=len(atomic_numbers), dim=0)
+        energy_terms.append(E_bond_angle_atomwise[:, None])
+
+        inputs["scalar_representation"] = torch.sum(torch.stack(energy_terms, dim=0), dim=0)
         return inputs
 
