6be847303d5eb313afa59515c670c4e8c64e47f3
Maltimore <git@maltimore.info>
2025-12-09 17:50:22 +0100

    atomistic/atomwise.py add NoOutNet

--- src/schnetpack/atomistic/atomwise.py
+++ src/schnetpack/atomistic/atomwise.py
@@ -8,7 +8,8 @@ import schnetpack as spk
 import schnetpack.nn as snn
 import schnetpack.properties as properties
 
-__all__ = ["Atomwise", "DipoleMoment", "Polarizability"]
+__all__ = ["Atomwise", "DipoleMoment", "Polarizability", "NoOutNet"]
+
 
 
 class Atomwise(nn.Module):
@@ -69,6 +70,76 @@ class Atomwise(nn.Module):
     def forward(self, inputs: Dict[str, torch.Tensor]) -> Dict[str, torch.Tensor]:
         # predict atomwise contributions
         y = self.outnet(inputs["scalar_representation"])
+
+        # accumulate the per-atom output if necessary
+        if self.per_atom_output_key is not None:
+            inputs[self.per_atom_output_key] = y
+
+        # aggregate
+        if self.aggregation_mode is not None:
+            idx_m = inputs[properties.idx_m]
+            maxm = int(idx_m[-1]) + 1
+            y = snn.scatter_add(y, idx_m, dim_size=maxm)
+            y = torch.squeeze(y, -1)
+
+            if self.aggregation_mode == "avg":
+                y = y / inputs[properties.n_atoms]
+
+        inputs[self.output_key] = y
+        return inputs
+
+
+class NoOutNet(Atomwise):
+    """
+    Predicts atom-wise contributions and accumulates global prediction, e.g. for the energy.
+
+    If `aggregation_mode` is None, only the per-atom predictions will be returned.
+    """
+
+    def __init__(
+        self,
+        n_in: int,
+        n_out: int = 1,
+        n_hidden: Optional[Union[int, Sequence[int]]] = None,
+        n_layers: int = 2,
+        activation: Callable = F.silu,
+        aggregation_mode: str = "sum",
+        output_key: str = "y",
+        per_atom_output_key: Optional[str] = None,
+    ):
+        """
+        Args:
+            n_in: input dimension of representation
+            n_out: output dimension of target property (default: 1)
+            n_hidden: size of hidden layers.
+                If an integer, same number of node is used for all hidden layers resulting
+                in a rectangular network.
+                If None, the number of neurons is divided by two after each layer starting
+                n_in resulting in a pyramidal network.
+            n_layers: number of layers.
+            aggregation_mode: one of {sum, avg} (default: sum)
+            output_key: the key under which the result will be stored
+            per_atom_output_key: If not None, the key under which the per-atom result will be stored
+        """
+        super(Atomwise, self).__init__()
+        self.output_key = output_key
+        self.model_outputs = [output_key]
+        self.per_atom_output_key = per_atom_output_key
+        if self.per_atom_output_key is not None:
+            self.model_outputs.append(self.per_atom_output_key)
+        self.n_out = n_out
+
+        if aggregation_mode is None and self.per_atom_output_key is None:
+            raise ValueError(
+                "If `aggregation_mode` is None, `per_atom_output_key` needs to be set,"
+                + " since no accumulated output will be returned!"
+            )
+
+        self.aggregation_mode = aggregation_mode
+
+    def forward(self, inputs: Dict[str, torch.Tensor]) -> Dict[str, torch.Tensor]:
+        # predict atomwise contributions
+        y = inputs["scalar_representation"]
 
         # accumulate the per-atom output if necessary
         if self.per_atom_output_key is not None:
